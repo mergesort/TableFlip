7d42ce3b8459d1d40589158d266b44e3fd1d524f
Joe Fabisevich <Joe@fabisevi.ch>
2019-11-10 12:44:14 -0500

    Adding Package.swift and updating podspec

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TableFlip",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "TableFlip",
+            targets: ["TableFlip"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "TableFlip",
+            dependencies: [])
+    ]
+)
